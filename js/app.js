fa082c7e4e60485be8979e0747fbcdf65e4ebd7a
Damian Kowal <damian@damiankowal.com>
2014-05-01 22:49:09 -0400

    Successfully use a promise to defer the rendering of data until the server response is received

--- js/app.js
+++ js/app.js
@@ -8,38 +8,30 @@ $( function() {
     app.getZip = function() {
         return '10011';
     }
+
     app.getMarkets = function( zip ) {
         // Source: USDA National Farmers Market Directory API
         // http://search.ams.usda.gov/farmersmarkets/v1/svcdesc.html
-        $.ajax({
+        return $.ajax({
             type: "GET",
             contentType: "application/json; charset=utf-8",
             url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
             dataType: 'jsonp',
-            jsonpCallback: 'app.markethResultsHandler'
         });
     };
 
-    app.marketResultsHandler = function( searchResults ) {
-        return searchResults.results;
-    };
-
-    app.renderMarkets = function( results ) {
+    app.renderMarkets = function( data ) {
         $results.append( app.marketTemplate({
-            list: results
+            list: data.results
         }));
     };
 
     app.init = function() {
         var zip = app.getZip(),
-            markets = app.getMarkets( zip );
+            marketsPromise = app.getMarkets( zip );
 
-        app.renderMarkets( markets );
+        marketsPromise.then( app.renderMarkets );
     };
-
-
-
-
 
     app.init();
 
