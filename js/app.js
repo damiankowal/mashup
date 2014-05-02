7ab38ce60408fb2f4d9b7ec5452de13c9a2357aa
Damian Kowal <damian@damiankowal.com>
2014-05-02 00:32:52 -0400

    Render the market list by chaining promises
    Not sure if using deferred statments this way is a good idea

--- js/app.js
+++ js/app.js
@@ -2,12 +2,38 @@ var app = app || {};
 
 $( function() {
     var $results = $( '#results' );
+    app.collections = {};
+    app.models = {};
+    app.views = {};
 
     app.marketTemplate = _.template( $( '#marketTemplate' ).html() );
 
+    app.models.Market = function( data ) {
+        // format: {"id":"1002192", "marketname":"1.7 Ballston FRESHFARM Market"}
+        this.id = data.id;
+        this.distance = data.marketname.substr( 0, data.marketname.indexOf( ' ' ) );
+        this.name = data.marketname.substr( data.marketname.indexOf( ' ' ) + 1 );
+    };
+
+    app.models.Market.prototype.render = function() {
+        return app.marketTemplate({
+            market: this
+        });
+    };
+
+    app.collections.Markets = function( markets ) {
+        this.items = markets;
+    };
+
+    app.collections.Markets.prototype.render = function() {
+        _.each( this.items, function( item ) {
+            $results.append( item.render() );
+        });
+    };
+
     app.getZip = function() {
         return '10011';
-    }
+    };
 
     app.getMarkets = function( zip ) {
         // Source: USDA National Farmers Market Directory API
@@ -20,17 +46,25 @@ $( function() {
         });
     };
 
-    app.renderMarkets = function( data ) {
-        $results.append( app.marketTemplate({
-            list: data.results
-        }));
+    app.buildMarketCollection = function( data ) {
+        var collection = [];
+        _.each( data.results, function( result ) {
+            collection.push( new app.models.Market( result ) );
+        });
+        return collection;
     };
 
     app.init = function() {
-        var zip = app.getZip(),
-            marketsPromise = app.getMarkets( zip );
+        var zip = app.getZip();
 
-        marketsPromise.then( app.renderMarkets );
+        app.getMarkets( zip )
+        .then( app.buildMarketCollection )
+        .then( function( marketList ) {
+            app.collections.markets = new app.collections.Markets( marketList );
+        })
+        .then( function() {
+            app.collections.markets.render();
+        });
     };
 
     app.init();
